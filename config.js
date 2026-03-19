cdc8612dbe468ac8dce307b339e5dcce9ac3c561
s4896537-byte <s4896537@gmail.com>
2026-03-19 23:46:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-const supabaseConfig = {
+// config.js
+const SUPABASE_CONFIG = {
     url: 'https://ouuaxqvjfzyapwutmqyf.supabase.co',
     anonKey: 'lmVUBPyAL2rhTfrAMyR2eg_JZ64dOjJ',
 };
 
-export default supabaseConfig;
+// 為了確保 app.js 能抓到，我們不使用 export，讓它成為全域變數
+window.SUPABASE_CONFIG = SUPABASE_CONFIG;
