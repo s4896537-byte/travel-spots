3eb8e8596f1d09137f56c13ac319caf4f4b84f05
s4896537-byte <s4896537@gmail.com>
2026-03-20 00:17:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const supabaseConfig = {
     url: 'https://ouuaxqvjfzyapwutmqyf.supabase.co',
-    anonKey: 'lmVUBPyAL2rhTfrAMyR2eg_JZ64dOjJ', // 建議確認一下 key 是否複製完整
+    anonKey: 'sb_publishable_lmVUBPyAL2rhTfrAMyR2eg_JZ64dOjJ', // 建議確認一下 key 是否複製完整
 };
 
 // 將設定掛載到 window，讓 app.js 可以抓到
